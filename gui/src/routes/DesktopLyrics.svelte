2f2489021eb2d647615caed2c492471b2b192902
LuoRain <luotianyi@luotianyi.me>
2026-06-10 13:14:39 +0800

    feat/desktoplrc: opacity support

--- gui/src/routes/DesktopLyrics.svelte
+++ gui/src/routes/DesktopLyrics.svelte
@@ -1,19 +1,50 @@
 <script lang="ts">
   import { Checkbox } from "$lib/components/ui/checkbox";
   import { Label } from "$lib/components/ui/label";
+  import { Slider } from "$lib/components/ui/slider";
 
   import * as settings from "$lib/settings";
 
+  let opacity = $state(100);
+  const opacityPromise = $state(
+    (settings.get("desktopLyrics.opacity") as Promise<number>).then((v) => {
+      opacity = v * 100;
+      return v;
+    })
+  );
+  let initalOpacityEffect = true;
+  $effect(() => {
+    void opacity;
+    if (initalOpacityEffect) {
+      initalOpacityEffect = false;
+      return;
+    }
+    settings.set("desktopLyrics.opacity", opacity / 100);
+  });
+
   let interpolatedLyricLinePromise = $state(
-    settings.get("desktopLyrics.interpolatedLyricLine") as Promise<
-      boolean | undefined
-    >
+    settings.get("desktopLyrics.interpolatedLyricLine") as Promise<boolean>
   );
 </script>
 
 <h1 class="text-2xl font-bold">桌面歌词</h1>
 <p class="mt-2 text-gray-700">深入定制 Open Orpheus 桌面歌词的行为。</p>
 
+{#await opacityPromise then}
+  <div class="my-4">
+    <p class="text-lg font-bold mb-2">桌面歌词透明度</p>
+    <div class="flex gap-4">
+      <Slider
+        type="single"
+        bind:value={opacity}
+        max={100}
+        step={1}
+        class="w-2/3"
+      />
+      <p>{Math.floor(opacity)}%</p>
+    </div>
+  </div>
+{/await}
 {#await interpolatedLyricLinePromise then interpolatedLyricLine}
   <Label
     class="mt-4 flex items-start gap-3 rounded-lg border p-3 hover:bg-accent/50 has-aria-checked:border-blue-600 has-aria-checked:bg-blue-50 dark:has-aria-checked:border-blue-900 dark:has-aria-checked:bg-blue-950"
